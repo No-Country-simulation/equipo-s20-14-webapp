3607bbbc4c7394fb7b61784684ccab4ebf99c97a
marcia <marcia.andrade.llanos@gmail.com>
2025-01-17 15:50:10 -0400

    commit inicial

---
+++ Frontend/front/src/FinanzasApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FinanzasApp = () => {
+    return (
+        <div className="bg-red-700 text-2xl">FinanzasApp</div>
+    )
+}
---
+++ Frontend/front/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      'exo2': 'Exo 2'
+    },
+    extend: {
+      colors: {
+        grayN: {
+          100: "#E4E4E5",
+          200: "#BEBEBE",
+          300: "#949494",
+          400: "#7A7A7A",
+          500: "#545454",
+          600: "#3C3C3C"
+        },
+        blueCard: {
+          100: "#024B6C"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+

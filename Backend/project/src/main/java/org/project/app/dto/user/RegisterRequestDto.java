355e404d2eb1f36e5de7216b3722599484c2142e
Luciano <lucianomolina970@gmail.com>
2025-01-21 21:02:46 -0300

    fix: update RegisterRequestDto.#6
    ---------

--- Backend/project/src/main/java/org/project/app/dto/user/RegisterRequestDto.java
+++ Backend/project/src/main/java/org/project/app/dto/user/RegisterRequestDto.java
@@ -3,6 +3,7 @@ package org.project.app.dto.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
 
@@ -12,6 +13,7 @@ public record RegisterRequestDto(
                 message = "El nombre de usuario debe tener al menos 3 letras y puede incluir espacios, apóstrofes o guiones"
         )
         @NotBlank(message = "El nombre de usuario no puede estar en blanco")
+        @Size(max = 50, message = "El nombre de usuario no puede exceder los 50 caracteres")
         String username,
 
         @Pattern(
@@ -20,6 +22,7 @@ public record RegisterRequestDto(
         )
         @Email(message = "El correo electrónico debe ser valido, utilizando ´@´")
         @NotBlank(message = "El correo electrónico no puede estar en blanco")
+        @Size(max = 100, message = "El correo electrónico no puede exceder los 100 caracteres")
         String email,
 
         @Pattern(
@@ -27,6 +30,7 @@ public record RegisterRequestDto(
                 message = "El contacto solo puede contener números positivos"
         )
         @NotBlank(message = "El contacto no puede estar en blanco")
+        @Size(max = 20, message = "El contacto no puede exceder los 20 caracteres")
         String contact,
 
         @NotBlank(message = "La contraseña no puede estar en blanco")
@@ -37,6 +41,7 @@ public record RegisterRequestDto(
                         contener al menos un dígito, una letra minúscula, una letra mayúscula,
                         un carácter especial (@#$%^&+=) y no debe tener espacios."""
         )
+        @Size(max = 64, message = "La contraseña no puede exceder los 64 caracteres")
         String password
 
 ) implements Serializable {

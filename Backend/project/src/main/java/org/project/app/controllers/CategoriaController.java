6a8aad9f57cad5c2a148f6e610a921bf47d8454f
David Alejandro Lugo Gámez <91472247+davidlugodev@users.noreply.github.com>
2025-01-28 21:46:08 -0400

    Update CategoriaController.java
    Utilizando CrearCategoriaDTO

--- Backend/project/src/main/java/org/project/app/controllers/CategoriaController.java
+++ Backend/project/src/main/java/org/project/app/controllers/CategoriaController.java
@@ -69,13 +69,13 @@ public class CategoriaController {
                     "finalmente persistir el nombre de la nueva categoria junto al usuario."
     )
     @PostMapping("/crear")
-    public ResponseEntity<String> crearCategoriaPersonal(@RequestBody CategoriaDTO categoriaDTO) {
+    public ResponseEntity<String> crearCategoriaPersonal(@RequestBody CrearCategoriaDTO crearCategoriaDTO) {
         //Obtiene Id del usuario que la está creando
-        Long usuarioId = categoriaDTO.getUsuario_Id();
+        Long usuarioId = crearCategoriaDTO.getUsuario_Id();
         return userRepository.findById(usuarioId).map(usuario -> {
-            String nombre = categoriaDTO.getNombre();
+            String nombre = crearCategoriaDTO.getNombre();
             Categoria categoria = categoriaService.crearCategoriaPersonal(nombre, usuario);
-            return ResponseEntity.ok(categoria.getNombre());
+            return ResponseEntity.ok(crearCategoriaDTO.getNombre());
         }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(""));
     }
 

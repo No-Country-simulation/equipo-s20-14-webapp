263b35fd191cf4ffaf19af36f9187e0e4dadd903
Luciano <lucianomolina970@gmail.com>
2025-01-30 11:04:34 -0300

    Update WebConfig2

--- Backend/project/src/main/java/org/project/app/config/WebConfig.java
+++ Backend/project/src/main/java/org/project/app/config/WebConfig.java
@@ -14,9 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
           .allowedOrigins("*")
-          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-          .allowedHeaders("*")
-          .allowCredentials(false)
-          .maxAge(3600);
+          .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }

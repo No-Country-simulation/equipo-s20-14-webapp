45a8d6bebcd8a37694a76061d8036b800548a206
Luciano <lucianomolina970@gmail.com>
2025-01-30 10:35:54 -0300

    update webconfig1

--- Backend/project/src/main/java/org/project/app/config/WebConfig.java
+++ Backend/project/src/main/java/org/project/app/config/WebConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * */
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {

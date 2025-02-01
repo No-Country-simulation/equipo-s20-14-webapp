4a192eb170701b259a9abc1b1de6a9dedbd6deab
Luciano <lucianomolina970@gmail.com>
2025-01-31 22:50:29 -0300

    Plan B 1.0

--- Backend/project/src/main/java/org/project/app/config/OpenApiConfig.java
+++ Backend/project/src/main/java/org/project/app/config/OpenApiConfig.java
@@ -13,8 +13,8 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("API Documentation: Clara").version("1.0"))
-//                .addServersItem(new Server().url("https://equipo-s20-14-webapp.onrender.com/"));
-                .addServersItem(new Server().url("http://localhost:8080/"));
+                .addServersItem(new Server().url("https://equipo-s20-14-webapp.onrender.com/"));
+//                .addServersItem(new Server().url("http://localhost:8080/"));
     }
 
 }

51be7075c5b25937dc1768310eda7a06962e031b
Luciano <lucianomolina970@gmail.com>
2025-02-02 10:56:23 -0300

    Cors3
    ----

--- Backend/project/src/main/java/org/project/app/security/SecurityConfiguration.java
+++ Backend/project/src/main/java/org/project/app/security/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-//                .cors(cors -> cors.configure(http)) // Habilitar CORS en Spring Security
+                .cors(cors -> cors.configure(http))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
